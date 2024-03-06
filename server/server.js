29e9431042763e9ea987540ebefa06cf5ece5e4e
Parida <divyaranjan.parida@acsicorp.com>
2024-03-06 13:42:01 +0530

    first api added

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+// express app
+const app = express();
+const port = process.env.PORT || 3000
+
+// routes
+app.get("/", (req,res)=>{
+    res.json({
+        msg:"Welllcooome...."
+    });
+})
+
+// listen for requests
+app.listen(port, ()=>{
+    console.log("listening on port", port);
+})
